01e9c2c05dddd4bc60183cc21a6253859d1bdd2b
vivienne white <vivihansf@gmail.com>
2017-05-08 14:35:15 -0500

    update some files

--- js/app.js
+++ js/app.js
@@ -1,6 +1,67 @@
 // wait for the DOM to finish loading
 $(document).ready(function() {
-  // all code to manipulate the DOM
-  // goes inside this function
+
+  var currPlayer = 1;
+  var moveCount = 0;
+
+//change the player to the other
+
+$('.box').on('click', function(){
+
+var squareSelected = $(this);
+
+if (squareSelected.hasClass('fa fa-times') || squareSelected.hasClass('fa fa-circle-o')) {
+  alert("This box has been checked!");
+} else {
+  if (currPlayer === 1) {
+    squareSelected.addClass('fa fa-times');
+    if(checkWon('fa fa-times')) {
+      alert('Congrats!');
+    } else {
+      currPlayer = 2;
+    }
+  } else {
+    squareSelected.addClass('fa fa-circle-o');
+    if(checkWon('fa fa-circle-o')) {
+      alert('Congrats!');
+    } else {
+      currPlayer = 1;
+    }
+  }
+  moveCount++;
+}
+});
+
+//display message
+function renderText(){
+  $('p').text('currPlayer:' + player)
+}
+//check if player has won
+function checkWon(currPlayer) {
+  console.log(moveCount);
+  if(($('#box1').hasClass(currPlayer) && $('#box2').hasClass(currPlayer)) && $('#box3').hasClass(currPlayer)){
+    return true;
+  } else if (($('#box4').hasClass(currPlayer) && $('#box5').hasClass(currPlayer)) && $('#box6').hasClass(currPlayer)) {
+    return true;
+  }  else if (($('#box7').hasClass(currPlayer) && $('#box8').hasClass(currPlayer)) && $('#box9').hasClass(currPlayer)) {
+    return true;
+  } else if (($('#box1').hasClass(currPlayer) && $('#box4').hasClass(currPlayer)) && $('#box7').hasClass(currPlayer)){
+    return true;
+  } else if (($('#box2').hasClass(currPlayer) && $('#box5').hasClass(currPlayer)) && $('#box8').hasClass(currPlayer)){
+    return true;
+  } else if (($('#box3').hasClass(currPlayer) && $('#box6').hasClass(currPlayer)) && $('#box9').hasClass(currPlayer)){
+    return true;
+  } else if (($('#box1').hasClass(currPlayer) && $('#box5').hasClass(currPlayer)) && $('#box9').hasClass(currPlayer)){
+    return true;
+  } else if (($('#box3').hasClass(currPlayer) && $('#box5').hasClass(currPlayer)) && $('#box7').hasClass(currPlayer)){
+    return true;
+  } else if(moveCount === 8) {
+    alert("It's a draw!");
+  };
+}
+//reset the gameboard
+$('.reset-button').on('click',function(){
+  $('.box').removeClass('fa fa-circle-o').removeClass('fa fa-times');
+});
 
 });
